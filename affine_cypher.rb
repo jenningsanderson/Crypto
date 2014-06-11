61a4cc8df251ba29f7fdab7e7355a6f34a3e1de4
Jennings Anderson <jennings.anderson@colorado.edu>
2014-06-11 15:54:27 -0600

    updated the comments -- unfortunately the decrypt function does not work for a != 1 because I do not calculate the invmod

--- affine_cypher.rb
+++ affine_cypher.rb
@@ -1,5 +1,3 @@
-
-
 #This simple affine cypher preserves puncuation and works on characters in the 0-25 space
 
 class AffineCypher
@@ -23,13 +21,16 @@ class AffineCypher
 				encrypted_string << char
 			end
 		end
-		encrypted_string.join
+		encrypted_string.join.downcase!
 	end
 
 
+	#Currently decryption only works for @a=1 (ie, a Caesarian shift cypher)
 	def decrypt(encrypted)
 		puts "Decrypt function received #{encrypted}"
 		decrypted_string = []
+
+		encrypted.upcase!
 
 		encrypted.chars.each do |char|
 			var = (char.ord - 65)
@@ -39,7 +40,7 @@ class AffineCypher
 				decrypted_string << char
 			end
 		end
-		decrypted_string.join
+		decrypted_string.join.downcase!
 	end
 
 end
@@ -48,9 +49,9 @@ if $0 == __FILE__
 
 	puts "Running the affine cypher"
 
-	cyph = AffineCypher.new(1,2)
+	cyph = AffineCypher.new(1,5)
 
-	string = "Hello World"
+	string = %Q{Hello World}
 
 	puts "The string is: #{string}"
 
